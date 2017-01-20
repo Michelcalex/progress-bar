26ea15880fa63c8ecbdc5c676b19f4705b7e5722
Michelcalex <Michelcalex+github@gmail.com>
2017-01-19 22:04:50 -0500

    Added progress bar info and hidden classes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,50 @@
 console.log('ready');
 
-window.addEventListener('load', function () {
-    var ProgressBar = require('progressbar.js')
+var ProgressBar = require('progressbar.js')
 
-    // Assuming we have an empty <div id="container"></div> in
-    // HTML
-    var test = document.getElementById('container');
-    var bar = new ProgressBar.Line(test, {easing: 'easeInOut'});
-    bar.animate(1);  // Value from 0.0 to 1.0
-});
+    var circleHeader = document.getElementById('container');
+    var bar = new ProgressBar.Circle(circleHeader, {
+    color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 2000,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 1 },
+    to: { color: '#333', width: 4 },
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+        circle.setText('');
+        } else {
+        circle.setText(value);
+        }
+
+    }
+    });
+    bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+    bar.text.style.fontSize = '2rem';
+
+    bar.animate(1.0);  // Number from 0.0 to 1.0
+
+    window.setInterval(function () {
+        let progressContainer = document.getElementById('container');
+        progressContainer.classList.add('hidden');
+
+        let howSection = document.getElementById('how-it-works');
+        howSection.classList.remove('hidden');
+
+        let exampleSection = document.getElementById('full-examples');
+        exampleSection.classList.remove('hidden');
+
+        let vocabSection = document.getElementById('vocab');
+        vocabSection.classList.remove('hidden');
+    }, 2000);
--- public/app.js
+++ public/app.js
@@ -1,15 +1,54 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 console.log('ready');
 
-window.addEventListener('load', function () {
-    var ProgressBar = require('progressbar.js')
+var ProgressBar = require('progressbar.js')
 
-    // Assuming we have an empty <div id="container"></div> in
-    // HTML
-    var test = document.getElementById('container');
-    var bar = new ProgressBar.Line(test, {easing: 'easeInOut'});
-    bar.animate(1);  // Value from 0.0 to 1.0
-});
+    var circleHeader = document.getElementById('container');
+    var bar = new ProgressBar.Circle(circleHeader, {
+    color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 2000,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 1 },
+    to: { color: '#333', width: 4 },
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+        circle.setText('');
+        } else {
+        circle.setText(value);
+        }
+
+    }
+    });
+    bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+    bar.text.style.fontSize = '2rem';
+
+    bar.animate(1.0);  // Number from 0.0 to 1.0
+
+    window.setInterval(function () {
+        let progressContainer = document.getElementById('container');
+        progressContainer.classList.add('hidden');
+
+        let howSection = document.getElementById('how-it-works');
+        howSection.classList.remove('hidden');
+
+        let exampleSection = document.getElementById('full-examples');
+        exampleSection.classList.remove('hidden');
+
+        let vocabSection = document.getElementById('vocab');
+        vocabSection.classList.remove('hidden');
+    }, 2000);
 
 },{"progressbar.js":4}],2:[function(require,module,exports){
 // Circle shaped progress bar
